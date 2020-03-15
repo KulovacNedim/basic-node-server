d95c882f8a5dc67a402111577077e2c27ca06abf
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-03-15 13:11:16 +0100

    staticly served files added

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
